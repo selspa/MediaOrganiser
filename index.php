1c7a927aacc108f26b53be9a5660bebe7cd5ae5a
selspa <49438543+selspa@users.noreply.github.com>
2021-12-16 09:31:14 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Media Organiser</title>
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/jquery.multiselect.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.js"></script>
+</head>
+
+<body>
+
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors','On');
+
+// include file to start database connection
+include_once('ADODB.php');
+
+
+
+?>
+
+<header>
+
+   <div id="header">
+    <h1>Media Organiser</h1>
+
+    <ul class="main-nav-bar">
+      <li><form id="search" action="search.php" method="post"><input id="search" type="text" value="" name="word-to-search" placeholder="Search..."></input><button type="submit" value="search" name="search"><i class="fa fa-search"></i></button></form></li>
+      <li><a href="mediafiles.php">Media Files</a> |</li>
+      <li><a href="playlists.php">Playlists</a></li>
+    </ul>
+  </div>
+
+</header>
+
+
+
+
+</body>
+
+
+<script type="text/javascript">
+   function playMyAudio(file){
+     document.getElementById(file).play();
+   }
+   function pauseMyAudio(file){
+     document.getElementById(file).pause();
+   }
+
+
+</script>
+
+</html>
